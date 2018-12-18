5d61b9dda57e7a6684c7911bf30dced1dbad0b0e
David Konerding <dek@insitro.com>
2018-12-17 16:01:01 -0800

    switch to logging.

--- mininwcostfree.py
+++ mininwcostfree.py
@@ -1,9 +1,12 @@
 ## a miniature implementation of Needleman Wunsch algorithm using three
 ## matrices M, Ix, Iy, and a backtrace matrix
 
-import sys, matrix, getopt, string
+import sys, matrix, getopt
+import logging
+import io
 
-debug = 0
+logger = logging.getLogger('mininwcostfree')
+logger.setLevel(logging.INFO)
 def readFASTA(file):
     f = open(file)
     sequence = []
@@ -17,7 +20,7 @@ def readFASTA(file):
                 break
             identifier = line[1:]
         else:
-            sequence.append(string.strip(line))
+            sequence.append(line.strip())
 
     return "".join(sequence)
 
@@ -28,25 +31,27 @@ def argmax(args):
     for i in range(len(args)):
         if best == None or args[i] > args[best]:
             best = i
-    print "Argmax", args, "best index: ", i, "best value: ", args[best]
+    logging.debug("Argmax %s best index: %d best value %d" % (args, i, args[best]))
     return best
 
 def printDPMatrix(s1, s2, m):
-	print "DP matrix"
-	sys.stdout.write("%4s %4s " % (" ", " "))
- 	for i in range(1, len(m)):
-            sys.stdout.write("%4s " % s1[i-1])
-	sys.stdout.write("\n")
-	
-	for i in range(len(m[0])):
-            if i == 0:
-                sys.stdout.write("%4s " % " ")
-            else:
-                sys.stdout.write("%4s " % s2[i-1])
+    s = io.StringIO()
+    s.write("DP matrix")
+    s.write("%4s %4s " % (" ", " "))
+    for i in range(1, len(m)):
+        s.write("%4s " % s1[i-1])
+    s.write("\n")
 
-            for j in range(len(m)):
-                sys.stdout.write("%4d " % m[j][i])
-            sys.stdout.write("\n")
+    for i in range(len(m[0])):
+        if i == 0:
+            s.write("%4s " % " ")
+        else:
+            s.write("%4s " % s2[i-1])
+
+        for j in range(len(m)):
+            s.write("%4d " % m[j][i])
+        s.write("\n")
+    logging.debug(s.getvalue())
 
 def build_align(sequence1, sequence2, M, Ix, Iy, Mbt, Ixbt, Iybt):
     aligned_seq1 = []
@@ -91,7 +96,7 @@ def build_align(sequence1, sequence2, M, Ix, Iy, Mbt, Ixbt, Iybt):
 
     i, j = bestIndex
 
-    reverse = lambda s: ''.join([s[i] for i in xrange(len(s)-1, -1, -1)])
+    reverse = lambda s: ''.join([s[i] for i in range(len(s)-1, -1, -1)])
     ## add any gapped suffix regions skipped by cost-free end-gap
     if i < len(sequence1):
         addseq = reverse(sequence1[i:])
@@ -111,29 +116,23 @@ def build_align(sequence1, sequence2, M, Ix, Iy, Mbt, Ixbt, Iybt):
 ##     import pdb
 ##     pdb.set_trace()
     while i > 0 or j > 0:
-        if debug:
-            print i, j
+        logger.debug("%d %d" % (i,j))
         ## if current matrix is match, emit the relevant sequence
         if id(matrix) == id(Mbt):
-            if debug:
-                print "Match matrix",
+            logger.debug("Match matrix")
             ## if either sequence has reached its left edge, gap it rather than using a negative index
             ## why would this be the case, ever?
             if i < 1:
-                if debug:
-                    print "emit s1 -",
+                logger.debug("emit s1 -")
                 s1 = "-"
             else:
-                if debug:
-                    print "emit s1", sequence1[i-1],
+                logger.debug("emit s1", sequence1[i-1])
                 s1 = sequence1[i-1]
             if j < 1:
-                if debug:
-                    print "emit s2 -"
+                logger.debug("emit s2 -")
                 s2 = "-"
             else:
-                if debug:
-                    print "emit s2", sequence2[j-1]
+                logger.debug("emit s2", sequence2[j-1])
                 s2 = sequence2[j-1]
 
             ## transition between matrices
@@ -146,42 +145,34 @@ def build_align(sequence1, sequence2, M, Ix, Iy, Mbt, Ixbt, Iybt):
             elif matrix[i][j] == 1:
                 if i > 0: i -= 1
                 if j > 0: j -= 1
-                if debug:
-                    print "Transition to Ixbt"
+                logger.debug("Transition to Ixbt")
                 matrix = Ixbt
             ##M->Iy
             elif matrix[i][j] == 2:
                 if i > 0: i -= 1
                 if j > 0: j -= 1
-                if debug:
-                    print "Transition to Iybt"
+                logger.debug("Transition to Iybt")
                 matrix = Iybt
 
         ## if current matrix is Ix (insert in second sequence) then emit a char from the first sequence and gap the second sequence
         elif id(matrix) == id(Ixbt):
-            if debug:
-                print "Ix matrix",
+            logger.debug("Ix matrix")
             s1 = sequence1[i-1]
             s2 = "-"
-            if debug:
-                print "emit s1", sequence1[i-1], "emit s2", "-"
+            logger.debug("emit s1 %s emit s2 -" % sequence1[i-1])
             if matrix[i][j] == 0:
-                if debug:
-                    print "Transition to Mbt"
+                logger.debug("Transition to Mbt")
                 matrix = Mbt
             i -= 1
 
         ## if current matrix is Iy (insert in first sequence) then gap the first sequence and emit a char from the second sequence
         elif id(matrix) == id(Iybt):
-            if debug:
-                print "Iy matrix"
+            logger.debug("Iy matrix")
             s1 = "-"
             s2 = sequence2[j-1]
-            if debug:
-                print "emit s1", "-", "emit s2", sequence2[j-1]
+            logger.debug("emit s1 - emit s2 %s" % sequence2[j-1])
             if matrix[i][j] == 0:
-                if debug:
-                    print "Transition to Mbt"
+                logger.debug("Transition to Mbt")
                 matrix = Mbt
             j -= 1
 
@@ -222,17 +213,14 @@ def nw(seq1, seq2, matrix, d, e):
         Iybt[0][i] = 1
 
 
-    if debug:
-        print "Initial"
-        printDPMatrix(seq1, seq2, M)
-        printDPMatrix(seq1, seq2, Ix)
-        printDPMatrix(seq1, seq2, Iy)
-        print ""
+    logger.debug("Initial")
+    printDPMatrix(seq1, seq2, M)
+    printDPMatrix(seq1, seq2, Ix)
+    printDPMatrix(seq1, seq2, Iy)
 
     for j in range(1, l_2):
         for i in range(1, l_1):
-            if debug:
-                print "Cell", i,j
+            logger.debug( "Cell %d %d" % (i,j))
             match = matrix[seq1[i-1],seq2[j-1]]
 
             set =  (  M[i-1][j-1] + match,
@@ -242,63 +230,50 @@ def nw(seq1, seq2, matrix, d, e):
             M[i][j] = set[setmax]
             Mbt[i][j] = setmax
 
-            if debug:
-                print
-                print "M M", seq1[i-1],seq2[j-1], "%d + %d = %d" % (M[i-1][j-1], match, M[i-1][j-1] + match)
-                print "IxM", seq1[i-1], "-", "%d + %d = %d" % (Ix[i-1][j-1], match, Ix[i-1][j-1] + match)
-                print "IyM", "-", seq2[j-1], "%d + %d = %d" % (Iy[i-1][j-1], match, Iy[i-1][j-1] + match )
-                print "Best M", setmax, set[setmax]
+            logger.debug("M M %s %s %d + %d = %d" % (seq1[i-1],seq2[j-1], M[i-1][j-1], match, M[i-1][j-1] + match))
+            logger.debug("IxM %s - %d + %d = %d" % (seq1[i-1], Ix[i-1][j-1], match, Ix[i-1][j-1] + match))
+            logger.debug("IyM - %s %d + %d = %d" % (seq2[j-1], Iy[i-1][j-1], match, Iy[i-1][j-1] + match))
+            logger.debug("Best M %s %s", setmax, set[setmax])
                 
-            set =  ( M[i-1][j] - d,\
+            set =  ( M[i-1][j] - d,
                     Ix[i-1][j] - e)
             setmax = argmax(set)
             Ix[i][j] = set[setmax]
             Ixbt[i][j] = setmax
-            if debug:
-                print
-                print "MI", seq1[i-1],"-", "%d + %d = %d" % (M[i-1][j], -d, M[i-1][j] - d)
-                print "II", seq1[i-1],"-", "%d + %d = %d" % (Ix[i-1][j], -e,Ix[i-1][j] - e )
-                print "Best Ix", setmax, set[setmax]
+            logger.debug("MI %s - %d + %d = %d" % (seq1[i-1], M[i-1][j], -d, M[i-1][j] - d))
+            logger.debug("II %s - %d + %d = %d" % (seq1[i-1],Ix[i-1][j], -e,Ix[i-1][j] - e))
+            logger.debug("Best Ix %s %s" % (setmax, set[setmax]))
 
             set =  ( M[i][j-1] - d,
                      Iy[i][j-1] - e)
             setmax = argmax((set))
             Iy[i][j] = set[setmax]
             Iybt[i][j] = setmax
-            if debug:
-                print
-                print "M Ix", "-", seq2[j-1], "%d + %d = %d" % (M[i][j-1], -d, M[i][j-1] - d)
-                print "IxIx", "-", seq2[j-1], "%d + %d = %d" % (Iy[i][j-1], -e, Iy[i][j-1] - e)
-                print "Best Iy", setmax, set[setmax]
-                print 
+            logger.debug("M Ix - %s %d + %d = %d" % (seq2[j-1], M[i][j-1], -d, M[i][j-1] - d))
+            logger.debug("IxIx - %s %d + %d = %d" % (seq2[j-1], Iy[i][j-1], -e, Iy[i][j-1] - e))
+            logger.debug("Best Iy %s %s" % (setmax, set[setmax]))
             
     if 1:
-        print ""
-        print "Final matrix"
+        logger.debug("Final matrix")
         printDPMatrix(seq1, seq2, M)
         printDPMatrix(seq1, seq2, Ix)
         printDPMatrix(seq1, seq2, Iy)
-        print ""
-        print "Final Traceback"
+        logger.debug("Final Traceback")
         printDPMatrix(seq1, seq2, Mbt)
         printDPMatrix(seq1, seq2, Ixbt)
         printDPMatrix(seq1, seq2, Iybt)
-        print ""
 
 
-    if debug:
-        print "Score:", M[l_1-1][l_2-1]
+    logger.debug("Score %d:", M[l_1-1][l_2-1])
 
-    if debug:
-        print ""
     aseq1, aseq2 = build_align(seq1, seq2, M, Ix, Iy, Mbt, Ixbt, Iybt)
     return aseq1, aseq2
 
 def printFASTA(queryFile, alignedQuery, subjectFile, alignedSubject, o=sys.stdout):
-    print >>o, ">%s" % queryFile
-    print >>o, alignedQuery
-    print >>o, ">%s" % subjectFile
-    print >>o, alignedSubject
+    print(">%s" % queryFile, file=o)
+    print(alignedQuery, file=o)
+    print(">%s" % subjectFile, file=o)
+    print(alignedSubject, file=o)
     
 def main():
     queryFile = None
@@ -308,8 +283,8 @@ def main():
     global debug
     try:
         opts, args = getopt.getopt(sys.argv[1:], "q:s:o:e:d",["query=", "subject=","openCost=","extendCost=","debug="])
-    except getopt.GetoptError, what:
-	raise RuntimeError, "usage"
+    except getopt.GetoptError(what):
+        raise RuntimeError("usage: %s" % str(what))
 
     for o, a in opts:
 
@@ -325,7 +300,7 @@ def main():
             debug = 1
 
     if not queryFile or not subjectFile:
-        raise RuntimeError, "No query or subject sequence supplied"
+        raise RuntimeError("No query or subject sequence supplied")
  
     alignedQuery, alignedSubject = nw(readFASTA(queryFile), readFASTA(subjectFile),  matrix.blosum50, openCost, extendCost)
     printFASTA(queryFile, alignedQuery, subjectFile, alignedSubject)
@@ -339,19 +314,19 @@ def test():
     #imatrix = matrix.makeIdentity(2, -1)
     imatrix = matrix.blosum50
 
-    seqpairs = [("".join(map(string.strip, open(sys.argv[1]).readlines()[1:])),
-                "".join(map(string.strip, open(sys.argv[2]).readlines()[1:])))]
-    print seqpairs
+    seqpairs = [("".join(map(str.strip, open(sys.argv[1]).readlines()[1:])),
+                "".join(map(str.strip, open(sys.argv[2]).readlines()[1:])))]
+    print(seqpairs)
 
     for seq1, seq2 in seqpairs:
         alignedQuery, alignedSubject = nw(seq1, seq2, imatrix, openCost, extendCost)
 
         printFASTA("seq1", alignedQuery, "seq2", alignedSubject)
 
-        if string.replace(alignedQuery, "-", "") != seq1:
-            print "First sequence was mangled"
-        if string.replace(alignedSubject, "-", "") != seq2:
-            print "Second sequence was mangled"
+        if alignedQuery.replace("-", "") != seq1:
+            print("First sequence was mangled")
+        if alignedSubject.replace("-", "") != seq2:
+            print("Second sequence was mangled")
         print
 
 if __name__ == '__main__':
