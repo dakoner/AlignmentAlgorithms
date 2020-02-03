c240c3e0c7936c098fad35072608f03443016a01
dek <dakoner@gmail.com>
2020-02-03 06:53:34 -0800

    Add script to print all alignments.

--- all.py
+++ all.py
@@ -1,17 +1,16 @@
 def recurse(s, t, i, j, s1, t1):
     # print(i, s[i], j, t[j], s1, t1)
-    if i == len(s):
+    if i == len(s) and j == len(t):
         print(''.join(s1))
         print(''.join(t1))
+        print()
         return
-    if j == len(t):
-        print(''.join(s1))
-        print(''.join(t1))
-        return
-    recurse(s, t, i+1, j, s1 + [s[i]], t1 + ['-'])
-    recurse(s, t, i, j+1, s1 + ['-'], t1 + [t[j]])
-    recurse(s, t, i+1, j+1, s1 + [s[i]], t1 + [t[j]])
-    print()
+    if i < len(s):
+        recurse(s, t, i+1, j, s1 + [s[i]], t1 + ['-'])
+    if j < len(t):
+        recurse(s, t, i, j+1, s1 + ['-'], t1 + [t[j]])
+    if i < len(s) and j < len(t):
+        recurse(s, t, i+1, j+1, s1 + [s[i]], t1 + [t[j]])
     
 
 s = "SMILES"
